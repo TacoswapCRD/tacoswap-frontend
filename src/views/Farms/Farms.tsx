66391ad4f73e15b569a8eaa981dbd4ae43f49733
TacoswapCRD <71548154+TacoswapCRD@users.noreply.github.com>
2020-10-01 22:43:05 +0200

    Update Farms.tsx

--- src/views/Farms/Farms.tsx
+++ src/views/Farms/Farms.tsx
@@ -27,7 +27,7 @@ const Farms: React.FC = () => {
             <Route exact path={path}>
               <PageHeader
                 icon={<img src={chef} height="120" />}
-                subtitle="Earn TACO tokens by staking Sushiswap SLP Tokens."
+                subtitle="Earn TACO tokens by staking Sushiswap SLP and UniSwapV2 Tokens."
                 title="Select Your Favorite Dishes"
               />
               <FarmCards />
